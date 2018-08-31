e2be1499b9d9412e3a0fb9255bd3d476f160e121
Zachary Scott <zachary.douglas.scott@gmail.com>
2018-08-31 17:08:18 -0400

    added some sql code to get it up and running

---
+++ db-code/testing-entries.sql
@@ -0,0 +1,15 @@
+INSERT INTO ers.ers_reimbursement_status
+(ers_reimb_status_id,ers_reimb_status) VALUES (0,'approved');
+INSERT INTO ers.ers_reimbursement_type
+(ers_reimb_type_id,ers_reimb_type) VALUES (0,'general');
+INSERT INTO ers.ers_user_roles
+(ers_user_role_id,user_role) VALUES (0,'manager');
+INSERT INTO ers.ers_users
+(ers_username,ers_password,user_first_name,user_last_name,user_email,user_role_id)
+VALUES ('zdscott','pass','Zachaary','Scott','zscott@evil-corp.net',0);
+
+INSERT INTO ers.ers_reimbursement
+            (reimb_amount,reimb_submitted,reimb_resolved,reimb_description,
+                reimb_author,reimb_resolver,reimb_status_id,reimb_type_id)
+                VALUES (100,CURRENT_TIMESTAMP, CURRENT_TIMESTAMP,'TESTING',
+						2,2,0,0)
